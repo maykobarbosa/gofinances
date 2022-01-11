acb5aa2818c6e047f2116542a3b7995f7db070f5
maykobarbosa <67514858+maykobarbosa@users.noreply.github.com>
2022-01-11 14:29:53 -0300

    Erro na splash

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,24 @@
+export default { 
+  colors: {
+    primary: '#5636D3',
+
+    secundary: '#FF872C',
+    secundary_light: 'rgba(255, 135, 44, 0.3)',
+
+    success: '#12A454',
+    success_light: 'rgba(18, 164, 84, 0.5)',
+
+    attention: '#e83f5b',
+    attention_light: 'rgba(232, 63 , 91, 0.3)',
+
+    shape: '#fff',
+    title: '#363F5F',
+    text: '#969CB3',
+    background: '#F0F2F5',
+  },
+  fonts: {
+    regular: 'Poppins_400Regular',
+    medium: 'Poppins_500Medium',
+    bold: 'Poppins_700Bold'
+  }
+}
