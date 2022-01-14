7e9d01c6d44faa5033cf5820a11c92c5ca491705
Mayko Barbosa <67514858+maykobarbosa@users.noreply.github.com>
2022-01-14 17:20:43 -0300

    vai

---
+++ src/components/Forms/Input/styles.ts
@@ -0,0 +1,7 @@
+import { TextInput } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+
+export const Container = styled(TextInput)`
+
+`;
